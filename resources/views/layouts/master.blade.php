92ac082912a0fe9dc4053de4cdf1bf9c5f5a6448
zarko gavric kubuntu <zarulja93@gmail.com>
2020-09-29 14:20:11 +0200

    Created custom classes in _variables.scss for change colors.

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -12,8 +12,8 @@ scratch. This page gets rid of all links and provides the needed markup only.
 
         <title>Laravel Application</title>
 
-       <link rel="stylesheet" href="/css/app.css">
-       <meta name="csrf-token" content="{{ csrf_token() }}">
+        <link rel="stylesheet" href="/css/app.css">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
     </head>
 
     <body class="hold-transition sidebar-mini">
@@ -74,7 +74,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
                with font-awesome or any other icon font library -->
                             <li class="nav-item">
                                 <router-link to="/dashboard" class="nav-link">
-                                    <i class="nav-icon fas fa-tachometer-alt"></i>
+                                    <i class="nav-icon fas fa-tachometer-alt blue"></i>
                                     <p>
                                         Dashboard
                                     </p>
@@ -83,7 +83,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
 
                             <li class="nav-item has-treeview">
                                 <a href="#" class="nav-link">
-                                    <i class="nav-icon fas fa-cog"></i>
+                                    <i class="nav-icon fas fa-cog green"></i>
                                     <p>
                                         Managment
                                         <i class="right fas fa-angle-left"></i>
@@ -106,19 +106,23 @@ scratch. This page gets rid of all links and provides the needed markup only.
                             </li>
                             <li class="nav-item">
                                 <router-link to="/profile" class="nav-link">
-                                    <i class="nav-icon fas fa-user-alt"></i>
+                                    <i class="nav-icon fas fa-user-alt orange"></i>
                                     <p>
                                         Profile
                                     </p>
                                 </router-link>
                             </li>
                             <li class="nav-item">
-                                <a href="#" class="nav-link">
-                                    <i class="nav-icon fas fa-power-off"></i>
-                                    <p>
-                                        Logout
-                                    </p>
-                                </a>
+                                <a class="nav-link" href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        <i class="nav-icon fas fa-power-off red"></i>
+                                        <p>{{ __('Logout') }}</p>
+                                    </a>
+
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                        @csrf
+                                    </form>
                             </li>
                         </ul>
                     </nav>
@@ -154,7 +158,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
         </div>
         <!-- ./wrapper -->
 
-       <script src="/js/app.js"></script>
+        <script src="/js/app.js"></script>
     </body>
 
 </html>
